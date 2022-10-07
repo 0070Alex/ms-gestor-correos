85c48fb95ec14cfd219fc9e4f0a480dbc37270ed
alex <78516248+0070Alex@users.noreply.github.com>
2022-10-06 23:29:59 -0600

    cors

--- gestorcorreos/src/main/java/com/dase/gestorcorreos/controllers/EmailController.java
+++ gestorcorreos/src/main/java/com/dase/gestorcorreos/controllers/EmailController.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins="http://localhost:4200")
-@RequestMapping("/email")
 @RestController
+@RequestMapping("/email")
 public class EmailController {
 
     @Autowired
